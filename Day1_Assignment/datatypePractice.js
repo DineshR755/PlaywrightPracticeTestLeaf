63b56cff47788e9521919923ab9bd12dda0428e9
Caped7Crusader <kkishoresenthilkumar@gmail.com>
2025-05-25 12:42:20 +0530

    day01_Assignment

---
+++ Day1_Assignment/datatypePractice.js
@@ -0,0 +1,28 @@
+// assigning and printing typeof firstName
+let firstName = "Dinesh"
+console.log(firstName);
+console.log("Type of firstName is " + typeof firstName);
+
+// assigning and printing typeof companyName
+let companyName = "Tech Mahindra"
+console.log(companyName);
+console.log("Type of companyName is " + typeof companyName);
+
+// assigning and printing typeof mobileNumber
+let mobileNumber = 6380068344
+console.log(mobileNumber);
+console.log("Type of mobileNumber is " + typeof mobileNumber);
+
+// assigning and printing typeof isAutomation
+let isAutomation = true
+console.log(isAutomation);
+console.log("Type of isAutomation is " + typeof isAutomation);
+
+// assigning and printing typeof hasPlaywright
+let hasPlaywright;
+console.log(hasPlaywright);
+console.log("Type of hasPlaywright is " + typeof hasPlaywright);
+
+
+
+
